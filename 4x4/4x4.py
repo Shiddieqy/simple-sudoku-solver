7b17f8199095a9721d6fb526709f2b28fb9fead1
Shiddieqy <shabriashiddieqy@gmail.com>
2021-01-24 16:23:05 +0700

    Finish 4x4.py

--- 4x4/4x4.py
+++ 4x4/4x4.py
@@ -14,36 +14,85 @@ for i in range (4):
 '''Create a null list that will be filled with index of array A which contain 
 a number in order to make it immutable'''
 C=[]
+D=[]
 for i in range(16):
     if (A[(i)//4][(i)%4] == 0):
         C.append(i)
-print(C)
-#isSolved variable : tells us is the sudoku solved or not
+    else :
+        D.append(i)
+
+'''#isSolved variable : tells us is the sudoku solved or not
 isSolved=0
 def isDone(Arr):
     m=0
     for i in range (4):
         for j in range (4):
             for k in range (j+1,4):
-                if Arr[i][k]==Arr[i][j] or Arr[k][i]==Arr[j][i] or Arr[(2*(k//2))+i//2][(2*(k%2))+i%2]==Arr[(2*(k//2))+j//2][(2*(k%2))+j%2]:
+                if Arr[i][k]==Arr[i][j] or Arr[k][i]==Arr[j][i] or Arr[(2*(i//2))+j//2][(2*(i%2))+j%2]==Arr[(2*(i//2))+k//2][(2*(i%2))+k%2]:
                     m=1
                     return False
     if m == 0 :
         return True
-#The main loop to solve the sudoku
-'''There are 2**(len(C) possible ways to fill the sudoku. This loop try each possibilities.
-And will stop if the sudoku solved. Or there is no more possibility left.'''
-for i in range(4**(len(C))):
-    for j in range(len(C)):
-        A[C[j]//4][C[j]%4]=((i//(4**j))%(4)+1)
-    if isDone(A):
-        print("The solution is : ")
-        for x in range(4):
-            for y in range(4):
-                print(A[x][y], end=" ")
-            print()
-        isSolved=1
-        break
-#If the loop stop without solving the sudoku, an error message will appear
-if isSolved==False :
+There is a problem with previous method. The maximum value of all the possibilities are too big. 
+Its about 4 billion. So i use different method'''
+
+rows = [set(range(1,5)) for i in range (4)]
+columns = [set(range(1,5)) for i in range (4)]
+squares = [set(range(1,5)) for i in range (4)]
+
+for i in D:
+    k = A[i//4][i%4]
+    rows[i//4].discard(k)
+    columns[i%4].discard(k)
+    squares[(i//4)//2*2+(i%4)//2].discard(k)
+F=[0,0,0]
+for i in range (4):
+    F[0]+=len(rows[i])
+    F[1]+=len(columns[i])
+    F[2]+=len(squares[i])
+
+if not len(C) == F[0] == F[1] == F[2] :
     print("Error, sudoku has no solution")
+
+else :
+    M=0
+    L=[0 for i in range(len(C))]
+    U=[0 for i in range(len(C))]
+    j=0
+    while j < len(C):
+        i = C[j]
+        x = rows[i//4] & columns[i%4] & squares[(((i//4)//2)*2)+((i%4)//2)]
+        y = list(x)
+        U[j]=len(y)
+        if len(y)==0:
+            m=0
+            for k in range (1,j+1):
+                l=C[j-k]
+                m+=1
+                rows[l//4].add(A[l//4][l%4])
+                columns[l%4].add(A[l//4][l%4])
+                squares[(((l//4)//2)*2)+((l%4)//2)].add(A[l//4][l%4])
+                if U[j-k] == L[j-k]:
+                    L[j-k]=0
+                else :
+                    L[j-k]+=1
+                    break
+            j-=m
+            continue 
+        else :
+            
+            A[i//4][i%4]=y[L[j]]
+            rows[i//4].remove(y[L[j]])
+            columns[i%4].remove(y[L[j]])
+            squares[(((i//4)//2)*2)+((i%4)//2)].remove(y[L[j]])
+            j+=1
+
+    print("the solution is: ")    
+    for i in range (4):
+        for j in range (4):
+            print(A[i][j],end=" ")
+            if j == 1:
+                print('|', end=" ")
+        print()
+        if i == 1 :
+            print("----------")
